36a9371bf11455979d0063322d00f9b60775da11
svgr110 <70746911+svgr110@users.noreply.github.com>
2020-09-21 13:40:31 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -40,7 +40,11 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-ap
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
 git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
 git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
-git clone https://github.com/user1121114685/koolproxyR.git package/lean/koolproxyR
+git clone https://github.com/user1121114685/koolproxyR.git package/lean/luci-app-koolproxyR
+svn co https://github.com/user1121114685/koolproxyR/ package/lean/luci-app-koolproxyR
+svn co https://github.com/user1121114685/koolproxyR/ package/lean/koolproxyR
+git clone  https://github.com/user1121114685/koolproxyR.git package/lean/koolproxyR
+
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
